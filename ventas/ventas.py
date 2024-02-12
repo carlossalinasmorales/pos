b2b8b5d1c9fff8645d17887db45d742ea2d77fa8
carlossalinasmorales <carlossalinasmorales1@gmail.com>
2024-02-12 20:03:54 -0300

    Avances en interfaz grafica de ventana ventas

--- ventas/ventas.py
+++ ventas/ventas.py
@@ -5,6 +5,30 @@ class VentasWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def admin(self):
+        print('Admin presionado')
+
+    def signout(self):
+        print('Sign Out presionado')
+
+    def pagar(self):
+        print('Pagar')
+    
+    def nueva_compra(self):
+        print('Nueva compra')
+
+    def eliminar_producto(self):
+        print('eliminar_producto presionado')
+        
+    def modificar_producto(self):
+        print('modificar_producto presionado')
+
+    def agregar_producto_codigo(self, codigo):
+        print('Se mando', codigo)
+    
+    def agregar_producto_nombre(self, nombre):
+        print('Se mando', nombre)
+
 class VentasApp(App):
     def build(self):
         return VentasWindow()
